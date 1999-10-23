7e58f86437bca66e1f3493d1d065ee13cb25907c
rlk <rlk>
1999-10-23 20:26:48 +0000

    Move LUT calculation to print-util

--- print.h
+++ print.h
@@ -1,5 +1,5 @@
 /*
- * "$Id: print.h,v 1.7 1999/10/21 01:27:37 rlk Exp $"
+ * "$Id: print.h,v 1.8 1999/10/23 20:26:48 rlk Exp $"
  *
  *   Print plug-in header file for the GIMP.
  *
@@ -207,6 +207,11 @@ extern void calc_hsv_to_rgb(guchar *rgb, double h, double s, double v);
 extern void calc_rgb_to_hsv(guchar *rgb, double *hue, double *sat,
 			    double *val);
 
+extern void compute_lut(lut_t *lut, lut16_t *lut16, int icontrast,
+			float red, float green, float blue, int ibrightness,
+			float print_gamma, float gimp_gamma, float user_gamma,
+			int linear, float printer_density, float user_density);
+
 /*
- * End of "$Id: print.h,v 1.7 1999/10/21 01:27:37 rlk Exp $".
+ * End of "$Id: print.h,v 1.8 1999/10/23 20:26:48 rlk Exp $".
  */
