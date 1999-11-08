b7d7fd3583a657419a00fe25433065b6ed945e1b
rlk <rlk>
1999-11-08 13:10:21 +0000

    Bug fix

--- print-escp2.c
+++ print-escp2.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-escp2.c,v 1.21 1999/11/07 22:18:51 rlk Exp $"
+ * "$Id: print-escp2.c,v 1.22 1999/11/08 13:10:21 rlk Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -31,6 +31,9 @@
  * Revision History:
  *
  *   $Log: print-escp2.c,v $
+ *   Revision 1.22  1999/11/08 13:10:21  rlk
+ *   Bug fix
+ *
  *   Revision 1.21  1999/11/07 22:18:51  rlk
  *   Support Stylus Photo
  *
@@ -1898,28 +1901,28 @@ escp2_unpack_2(int length,
       if (!(i & 1))
 	{
 	  *outlo =
-	    (inbyte & (1 << 7) << 0) +
-	    (inbyte & (1 << 5) << 1) +
-	    (inbyte & (1 << 3) << 2) +
-	    (inbyte & (1 << 1) << 3);
+	    ((inbyte & (1 << 7)) << 0) +
+	    ((inbyte & (1 << 5)) << 1) +
+	    ((inbyte & (1 << 3)) << 2) +
+	    ((inbyte & (1 << 1)) << 3);
 	  *outhi =
-	    (inbyte & (1 << 6) << 1) +
-	    (inbyte & (1 << 4) << 2) +
-	    (inbyte & (1 << 2) << 3) +
-	    (inbyte & (1 << 0) << 4);
+	    ((inbyte & (1 << 6)) << 1) +
+	    ((inbyte & (1 << 4)) << 2) +
+	    ((inbyte & (1 << 2)) << 3) +
+	    ((inbyte & (1 << 0)) << 4);
 	}
       else
 	{
 	  *outlo +=
-	    (inbyte & (1 << 1) >> 1) +
-	    (inbyte & (1 << 3) >> 2) +
-	    (inbyte & (1 << 5) >> 3) +
-	    (inbyte & (1 << 7) >> 4);
+	    ((inbyte & (1 << 1)) >> 1) +
+	    ((inbyte & (1 << 3)) >> 2) +
+	    ((inbyte & (1 << 5)) >> 3) +
+	    ((inbyte & (1 << 7)) >> 4);
 	  *outhi +=
-	    (inbyte & (1 << 0) >> 0) +
-	    (inbyte & (1 << 2) >> 1) +
-	    (inbyte & (1 << 4) >> 2) +
-	    (inbyte & (1 << 6) >> 3);
+	    ((inbyte & (1 << 0)) >> 0) +
+	    ((inbyte & (1 << 2)) >> 1) +
+	    ((inbyte & (1 << 4)) >> 2) +
+	    ((inbyte & (1 << 6)) >> 3);
 	  outlo++;
 	  outhi++;
 	}
@@ -2058,5 +2061,5 @@ escp2_write_weave(FILE          *prn,	/* I - Print file or command */
 }
 
 /*
- * End of "$Id: print-escp2.c,v 1.21 1999/11/07 22:18:51 rlk Exp $".
+ * End of "$Id: print-escp2.c,v 1.22 1999/11/08 13:10:21 rlk Exp $".
  */
