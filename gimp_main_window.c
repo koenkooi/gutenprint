193844c3aec1586f8c50810119209fe3fffcb5eb
jmv <jmv>
2000-06-19 21:33:49 +0000

    Stupid bug fix in adding Unit to gim_main_window

--- gimp_main_window.c
+++ gimp_main_window.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: gimp_main_window.c,v 1.20 2000/06/19 16:40:39 jmv Exp $"
+ * "$Id: gimp_main_window.c,v 1.21 2000/06/19 21:33:49 jmv Exp $"
  *
  *   Main window code for Print plug-in for the GIMP.
  *
@@ -1109,31 +1109,31 @@ gimp_position_callback (GtkWidget *widget)
   if (widget == top_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       vars.top = ((new_value + 1.0 / 144) * 72) - top;
     }
   else if (widget == left_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       vars.left = ((new_value + 1.0 / 144) * 72) - left;
     }
   else if (widget == bottom_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       vars.top = ((new_value + 1.0 / 144) * 72) - (top + print_height);
     }
   else if (widget == right_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       vars.left = ((new_value + 1.0 / 144) * 72) - (left + print_width);
     }
   else if (widget == width_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       if (vars.scaling >= 0) {
         gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (scaling_ppi), TRUE);
         gimp_scaling_callback (scaling_ppi);
@@ -1144,7 +1144,7 @@ gimp_position_callback (GtkWidget *widget)
   else if (widget == height_entry)
     {
       gfloat new_value = atof (gtk_entry_get_text (GTK_ENTRY (widget)));
-	  value *= unit_scaler;
+	  new_value *= unit_scaler;
       if (vars.scaling >= 0) {
         gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (scaling_ppi), TRUE);
         gimp_scaling_callback (scaling_ppi);
