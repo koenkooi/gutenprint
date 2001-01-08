32852204dcd8e7720dc5bb162f4dd3518100ccca
easysw <easysw>
2001-01-08 21:31:07 +0000

    Stylus Color 680/777 support in 4.0.x tree.

--- print-escp2.c
+++ print-escp2.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-escp2.c,v 1.265 2000/10/18 00:52:34 rlk Exp $"
+ * "$Id: print-escp2.c,v 1.265.2.1 2001/01/08 21:31:07 easysw Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -1048,7 +1048,17 @@ static escp2_printer_t model_capabilities[] =
     { 2.0, 1.3, 1.3, .646, .646, .323, .323, .1615, .0808 },
     &simple_6color_inks
   },
-
+  /* 32: Stylus Color 680/777 */
+  {
+    (MODEL_INIT_STANDARD | MODEL_HASBLACK_YES | MODEL_INK_NORMAL
+     | MODEL_6COLOR_NO | MODEL_720DPI_600 | MODEL_VARIABLE_MULTI
+     | MODEL_COMMAND_1999 | MODEL_GRAYMODE_YES | MODEL_1440DPI_YES
+     | MODEL_ROLLFEED_NO | MODEL_ZEROMARGIN_NO),
+    48, 6, 48, 6, 360, 360, INCH(17 / 2), INCH(14), 9, 9, 0, 1, 1, 0,
+    { -1, 1, 0x11, 1, 0x10, -1, 0x10 },
+    { 2.0, 1.3, 1.3, .646, .710, .323, .365, .1825, .0913 },
+    &variable_3pl_inks
+  },
 };
 
 typedef struct escp_init
