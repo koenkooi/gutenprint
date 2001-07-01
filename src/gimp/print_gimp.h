ddf8f2bb615beb7a093350e641e90ae68aa0e216
rlk <rlk>
2001-07-01 20:08:11 +0000

    Clean up the duplication between vars and the printer list.  This may allow us to further simplify the logic in gimp_main_window.c; that needs some investigation.

--- src/gimp/print_gimp.h
+++ src/gimp/print_gimp.h
@@ -1,5 +1,5 @@
 /*
- * "$Id: print_gimp.h,v 1.14 2001/07/01 13:19:33 rlk Exp $"
+ * "$Id: print_gimp.h,v 1.15 2001/07/01 20:08:11 rlk Exp $"
  *
  *   Print plug-in for the GIMP.
  *
@@ -67,6 +67,9 @@ typedef struct		/**** Printer List ****/
 
 #define THUMBNAIL_MAXW	(128)
 #define THUMBNAIL_MAXH	(128)
+#define MIN_CUSTOM_WIDTH (1 * 72)
+#define MIN_CUSTOM_HEIGHT (2 * 72)
+
 
 extern gint    thumbnail_w, thumbnail_h, thumbnail_bpp;
 extern guchar *thumbnail_data;
@@ -87,6 +90,7 @@ extern gint             saveme;
 
 extern GtkWidget *gimp_color_adjust_dialog;
 extern GtkWidget *dither_algo_combo;
+extern stp_vars_t *pv;
 
 /*
  * Function prototypes
@@ -110,7 +114,6 @@ extern void gimp_plist_build_combo         (GtkWidget      *combo,
 extern void gimp_do_color_updates    (void);
 extern void gimp_redraw_color_swatch (void);
 extern void gimp_build_dither_combo  (void);
-extern void gimp_build_printer_combo (void);
 extern void gimp_create_color_adjust_window  (void);
 extern void gimp_update_adjusted_thumbnail   (void);
 extern void gimp_create_main_window (void);
