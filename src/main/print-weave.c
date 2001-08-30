22e61d52d66fa71b77894abb568ee949f56083a5
rlk <rlk>
2001-08-30 23:23:57 +0000

    Produce better output from assertion failures

--- src/main/print-weave.c
+++ src/main/print-weave.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-weave.c,v 1.33 2001/08/04 13:56:15 rlk Exp $"
+ * "$Id: print-weave.c,v 1.34 2001/08/30 23:23:57 rlk Exp $"
  *
  *   Softweave calculator for gimp-print.
  *
@@ -53,9 +53,18 @@
 #endif
 
 #ifdef ASSERTIONS
-#define assert(x,v) if (!(x)) { stp_eprintf(v, "ASSERTION FAILURE!  \"%s\", line %d.\n", __FILE__, __LINE__); exit(1); }
+#define assert(x,v)							\
+do									\
+{									\
+  if (!(x))								\
+    {									\
+      stp_eprintf(v, "Assertion %s failed! file %s, line %d.\n",	\
+		  #x, __FILE__, __LINE__);				\
+      exit(1);								\
+    }									\
+} while (0)
 #else
-#define assert(x,v) /* nothing */
+#define assert(x,v) do {} while (0)
 #endif
 
 #ifdef TEST
