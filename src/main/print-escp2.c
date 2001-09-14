a3165c0e038c564af9976577397773305ea0edd7
rlk <rlk>
2001-09-14 11:00:08 +0000

    Typo!

--- src/main/print-escp2.c
+++ src/main/print-escp2.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-escp2.c,v 1.113 2001/09/14 02:12:34 rlk Exp $"
+ * "$Id: print-escp2.c,v 1.114 2001/09/14 11:00:08 rlk Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -3001,7 +3001,7 @@ escp2_parameters(const stp_printer_t printer,	/* I - Printer model */
       unsigned int pheight = stp_papersize_get_height(pt);
       if (strlen(stp_papersize_get_name(pt)) > 0 &&
 	  pwidth <= width_limit && pheight <= height_limit &&
-	  (pheight >= min_width_limit || pheight == 0) &&
+	  (pheight >= min_height_limit || pheight == 0) &&
 	  (pwidth >= min_width_limit || pwidth == 0) &&
 	  (pwidth == 0 || pheight > 0 ||
 	   escp2_has_cap(model, MODEL_ROLLFEED, MODEL_ROLLFEED_YES, v)))
