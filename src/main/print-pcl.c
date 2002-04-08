fa8cd01d4da288227ed74139cc9baae02aa32a36
davehill <davehill>
2002-04-08 09:25:32 +0000

    Corrected margins on DeskJet 2000 per mail from Michel Goraczko to Roger Leigh.

--- src/main/print-pcl.c
+++ src/main/print-pcl.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-pcl.c,v 1.51 2002/04/03 00:27:25 rlk Exp $"
+ * "$Id: print-pcl.c,v 1.52 2002/04/08 09:25:32 davehill Exp $"
  *
  *   Print plug-in HP PCL driver for the GIMP.
  *
@@ -941,7 +941,7 @@ static const pcl_cap_t pcl_model_capabilities[] =
     17 * 72 / 2, 14 * 72,
     1, 1,				/* Min paper size */
     PCL_RES_150_150 | PCL_RES_300_300 | PCL_RES_600_600,
-    12, 12, 18, 18,
+    0, 35, 18, 18,			/* Michel Goraczko */
     PCL_COLOR_CMYK,
     PCL_PRINTER_DJ | PCL_PRINTER_NEW_ERG | PCL_PRINTER_TIFF | PCL_PRINTER_MEDIATYPE |
       PCL_PRINTER_CUSTOM_SIZE | PCL_PRINTER_BLANKLINE,
