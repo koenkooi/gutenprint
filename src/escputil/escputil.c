fd74ed47098ba639003e17505240612775cb1fa9
rlk <rlk>
2003-09-21 02:39:03 +0000

    Preliminary support for Epson Stylus C63/C64 and PX-V500

--- src/escputil/escputil.c
+++ src/escputil/escputil.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: escputil.c,v 1.39.2.13 2003/09/11 00:20:55 rlk Exp $"
+ * "$Id: escputil.c,v 1.39.2.14 2003/09/21 02:39:03 rlk Exp $"
  *
  *   Printer maintenance utility for EPSON Stylus (R) printers
  *
@@ -210,6 +210,8 @@ stp_printer_t printer_list[] =
   { "C60",	N_("Stylus C60"),	3,	15,	0,	0,	0 },
   { "C61",	N_("Stylus C61"),	3,	15,	0,	0,	0 },
   { "C62",	N_("Stylus C62"),	3,	15,	0,	0,	0 },
+  { "C63",	N_("Stylus C63"),	4,	15,	0,	1,	7 },
+  { "C64",	N_("Stylus C64"),	4,	15,	0,	1,	7 },
   { "C70",	N_("Stylus C70"),	4,	15,	0,	1,	7 },
   { "C80",	N_("Stylus C80"),	4,	15,	0,	1,	7 },
   { "C82",	N_("Stylus C82"),	4,	15,	0,	1,	7 },
